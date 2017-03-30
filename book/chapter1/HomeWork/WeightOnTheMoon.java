04ded7fad890c69a02154d68f6c2039147b2cb05
Magnumv44 <vitaliy1984@gmail.com>
2017-03-30 21:04:56 +0300

    Сделано первое задание и подготовлен файл для второго.

---
+++ book/chapter1/HomeWork/WeightOnTheMoon.java
@@ -0,0 +1,20 @@
+/*
+ * Задание:
+ * Сила тяжести на Луне составляет около 17% земной. Напишите программу, которая вычислила бы ваш вес на Луне.
+ *
+ * Файлу с исходным кодом присвойте имя WeightOnTheMoon.java
+ */
+class WeightOnTheMoon {
+	public static void main(String[] args) {
+		double my_weight = 82; // Мой вес
+		double moon_gravity = 17; // Сила притяжения на Луне
+		double earth_gravity = 100; // Сила притяжение на Земле
+		double moon_weight; // Переменная которая будет хврнить в себе вес на Луне
+
+		//Производим расчет веса на Луне
+		moon_weight = (moon_gravity / earth_gravity) * my_weight;
+
+		// Выводим на эекран результат
+		System.out.print("Мой вес на Луне равен: " + moon_weight + " кг.");
+	}
+}
