48dcb098c1b6082adf9a683e80a9ba6aa943e6e8
Magnumv44 <vitaliy1984@gmail.com>
2018-07-19 17:33:39 +0300

    Добавлены первый и второй примеры 4-й главы

---
+++ book/chapter4/TwoVehicles/TwoVehicles.java
@@ -0,0 +1,44 @@
+/*
+ * Программа денстрирующая принцип создания класса.
+ * Который будет иметь два его объкта.
+ *
+ * @author Vitaliy Magnum
+ */
+class Vehicle {
+    int passengers; // Количество пассажиров
+    int fuelcap;    // Емкость топливного бака
+    int mpg;        // потребление топлива в милях на галон
+}
+
+/**
+ * Класс в котором создается два объекта класса Vehicle.
+ */
+class TwoVehicles {
+	public static void main(String[] args) {
+		Vehicle minivan = new Vehicle();
+		Vehicle sportcar = new Vehicle();
+
+		int range1, range2;
+
+		// Присваиваем значения полям в объекте minivan
+		minivan.passengers = 7;
+		minivan.fuelcap = 16;
+		minivan.mpg = 21;
+
+		// Присваиваем значения полям в объекте minivan
+		sportcar.passengers = 2;
+		sportcar.fuelcap = 14;
+		sportcar.mpg = 12;
+
+		//Расчитываем дальность поездки для обоих объектов
+		range1 = minivan.fuelcap * minivan.mpg;
+		range2 = sportcar.fuelcap * sportcar.mpg;
+
+		System.out.println("Мини-фургон может перевезти " +
+			minivan.passengers + " пассажиров на росстояние " + range1 +
+			" миль");
+		System.out.println("Спортивный автомобиль может перевезти " +
+			sportcar.passengers + " пассажиров на росстояние " + range2 +
+			" миль");
+	}
+}
---
+++ book/chapter4/VehicleDemo/VehicleDemo.java
@@ -0,0 +1,32 @@
+/*
+ * Программа денстрирующая принцип создания класса и его объкта.
+
+ * @author Vitaliy Magnum
+ */
+class Vehicle {
+    int passengers; // Количество пассажиров
+    int fuelcap;    // Емкость топливного бака
+    int mpg;        // потребление топлива в милях на галон
+}
+
+/**
+ * Класс в котором создается объект класса Vehicle.
+ */
+class VehicleDemo {
+	public static void main(String[] args) {
+		Vehicle minivan = new Vehicle();
+		int range;
+
+		// присваиваем значение полям (переменным) в объекте minivan
+		minivan.passengers = 7;
+		minivan.fuelcap = 16;
+		minivan.mpg = 21;
+
+		// Рассчитываем дальность поездки при полном баке
+		range = minivan.fuelcap * minivan.mpg;
+
+		System.out.println("Мини-фургон может перевезти " +
+			minivan.passengers + " пассажиров на росстояние " + range +
+			" миль");
+	}
+}
