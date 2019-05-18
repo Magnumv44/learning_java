725d2903fee1cb09d90db0b5b26bf151ee6e467d
Magnumv44 <vitaliy1984@gmail.com>
2019-05-18 22:30:48 +0300

    Изменет пример с учетом правил инкапсулирования

--- book/chapter6/CallByRef/CallByRef.java
+++ book/chapter6/CallByRef/CallByRef.java
@@ -8,10 +8,10 @@ class CallByref {
     public static void main(String[] args) {
         Test ob = new Test(15, 20);
 
-        System.out.println("Значение переменных ob.a и ob.b перед вызовом метода change: " + ob.a + ", " + ob.b);
+        System.out.println("Значение переменных ob.a и ob.b перед вызовом метода change: " + ob.getA() + ", " + ob.getB());
 
         ob.change(ob);
 
-        System.out.println("Значение переменных ob.a и ob.b после вызова метода change: " + ob.a + ", " + ob.b);
+        System.out.println("Значение переменных ob.a и ob.b после вызова метода change: " + ob.getA() + ", " + ob.getB());
     }
 }
--- book/chapter6/CallByRef/Test.java
+++ book/chapter6/CallByRef/Test.java
@@ -4,17 +4,25 @@
  * @author Magnuym
  */
 class Test {
-    public int a, b;
+    private int a, b;
 
     public Test(int a, int b) {
         this.a = a;
         this.b = b;
     }
 
+    public int getA() {
+        return a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
     /*
-     Метод принимающий в качестве параметра объект.
-     Переменные ob.a и ob.b у переданно объекта так же будут изменятся при вызове метода.
-     */
+         Метод принимающий в качестве параметра объект.
+         Переменные ob.a и ob.b у переданно объекта так же будут изменятся при вызове метода.
+         */
     public void change(Test ob) {
         ob.a = ob.a + ob.b;
         ob.b = -ob.b;
