04bc24d868283c383600d4738abaefa47d71630c
Magnumv44 <vitaliy1984@gmail.com>
2019-11-01 14:59:34 +0200

    Добавлен пример демонстрирующий второй вариант примененеия ключевого слова super.
    Дает доступ к полям суперкласса если они имеют такое же имя как и переменные подклюсса

---
+++ book/chapter7/AccessToFieldOfSuperclass/UseSuper.java
@@ -0,0 +1,12 @@
+/*
+ * Клас содержащий метод main для запуска програмы и теста класса B.
+ *
+ * @author Magnum
+ */
+class UseSuper {
+    public static void main(String[] args) {
+        B subOb = new B(1, 2);
+
+        subOb.show();
+    }
+}
