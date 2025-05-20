cb25b0f8e3bea417356fb1023f27bc738d5da622
Magnumv44 <vitaliy1984@gmail.com>
2025-05-20 16:13:49 +0300

    Додано приклад роботи оператора switch

---
+++ book/chapter3/src/space/magnum/examples/SwitchDemo/SwitchDemo.java
@@ -0,0 +1,33 @@
+package space.magnum.examples.SwitchDemo;
+
+/**
+ * Програма SwitchDemo.
+ * Демонстрація роботи керуючого оператора switch.
+ */
+public class SwitchDemo {
+    public static void main(String[] args) {
+        int i;
+
+        for (i = 0; i < 10; i++) {
+            switch (i) {
+                case 0:
+                    System.out.println("i дорiвнює нулю");
+                    break;
+                case 1:
+                    System.out.println("i дорiвнює одиницi");
+                    break;
+                case 2:
+                    System.out.println("i дорiвнює двом");
+                    break;
+                case 3:
+                    System.out.println("i дорiвнює трьом");
+                    break;
+                case 4:
+                    System.out.println("i дорiвнює чотирьом");
+                    break;
+                default:
+                    System.out.println("i дорiвнює або бiльше п'яти");
+            }
+        }
+    }
+}
