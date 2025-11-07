16fe6d8e7dabcc0ba5ff83f406d44b78fbe159dc
Magnumv44 <vitaliy1984@gmail.com>
2025-11-07 14:45:39 +0200

    Create EmptyPartLoopCondition3.java

---
+++ book/chapter3/src/space/magnum/examples/EmptyPartLoopCondition/EmptyPartLoopCondition3.java
@@ -0,0 +1,17 @@
+package space.magnum.examples.EmptyPartLoopCondition;
+
+/**
+ * Файл класу EmptyPartLoopCondition3.
+ * Програма демонструє роботу циклу без тіла циклу.
+ */
+public class EmptyPartLoopCondition3 {
+    public static void main(String[] args) {
+        int i; // оголошуємо ініціалізуючу змінну за межами умови циклу
+        int sum = 0; // змінна для підрахунку суми кількостей ітерацій
+
+        for (i = 1; i <= 5; sum += i++) ; // в цьому циклі відсутне тіло циклу, тоб то ніякого коду в середині не виконується
+
+        System.out.println("Кількість ітерацій: " + i);
+        System.out.println("Сума кількостей ітерацій: " + sum);
+    }
+}
